99be0f5b8edd9b2b0d1d3d2f28d6a32c6eddcf8c
john mbugua <johnymbugua1997@gmail.com>
2024-06-21 09:22:07 +0300

    setting up filters for numeric values and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,18 @@ const ProductModel = require("../models/product");
 
 const getProducts = asyncWrapper(async (req, res) => {
   //    code to find product from the databse and send back the response
-  const products = await ProductModel.find({});
+  const products = await ProductModel.find({ price: { $gt: 100 } })
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(1);
 
-  res.status(200).json({ products });
+  res.status(200).json({ products, inHibit: products.length });
 });
 
 const getProductStatic = asyncWrapper(async (req, res) => {
   //    code to find product from the databse and send back the response
-  const { featured, company, name, sort} = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const objectQuery = {};
 
   if (featured) {
@@ -21,25 +25,68 @@ const getProductStatic = asyncWrapper(async (req, res) => {
     objectQuery.company = company;
   }
 
-  if(name) {
+  if (name) {
     // objectQuery.name = name;
     // using mongodb query params
-    objectQuery.name = {$regex: name, $options: "i"}
+    objectQuery.name = { $regex: name, $options: "i" };
   }
 
-   let results = ProductModel.find(objectQuery);
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
 
-   if(sort) {
-     const chainedSorted = sort.split(',').join(' ');
-     console.log(chainedSorted);
-     results = results.sort(chainedSorted);
-   }else{
-    results = results.sort('createdAt')
-   }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
 
-   const products = await results
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
 
-  // const products = await ProductModel.find(objectQuery).limit(10).sort({name: -1});
+      if (options.includes(field)) {
+        objectQuery[field] = { [operator]: Number(value) };
+      }
+    });
+
+    console.log(objectQuery);
+  }
+
+  let results = ProductModel.find(objectQuery);
+
+  // code for sorting filtering
+
+  if (sort) {
+    const chainedSorted = sort.split(",").join(" ");
+    console.log(chainedSorted);
+    results = results.sort(chainedSorted);
+  } else {
+    results = results.sort("createdAt");
+  }
+
+  // code for field filtering
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    results = results.select(fieldList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  results = results.skip(skip).limit(limit);
+
+  console.log(page);
+  console.log(limit);
+  const products = await results;
+
+  // const products = await ProductModel.find(objectQuery).limit(10).sort({name: -1}).limit(10).select('name price').skip(5);
   // console.log(products);
   res.status(200).json({ products, iniBit: products.length });
 });
